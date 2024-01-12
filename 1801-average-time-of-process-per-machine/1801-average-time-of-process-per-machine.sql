9f5c8978a37f8d4c114c10c3af41616cb2eb1a55
Kareem Hossam <geek.kareem.hossam@gmail.com>
2024-01-12 09:58:43 +0200

    Time: 489 ms (30.37%), Space: 0 (100.00%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,16 +1,10 @@
 # Write your MySQL query statement below
 SELECT
-	machine_id,
-	ROUND(AVG(timestamp_diff), 3) AS processing_time
-FROM(
-	SELECT
 	a1.machine_id,
-    ABS(a1.timestamp - a2.timestamp) AS timestamp_diff
-	FROM activity a1
-	JOIN activity a2
-	WHERE a1.machine_id = a2.machine_id
-	AND a1.process_id = a2.process_id
-	AND a1.activity_type != a2.activity_type
-	GROUP BY a1.machine_id, a1.process_id
-) AS result
+	ROUND(AVG(ABS(a1.timestamp - a2.timestamp)), 3) AS processing_time
+FROM activity a1
+JOIN activity a2
+WHERE a1.machine_id = a2.machine_id
+AND a1.process_id = a2.process_id
+AND a1.activity_type != a2.activity_type
 GROUP BY machine_id
