b119ace9e2827087ad03ff7183821214ef6d86ff
Kareem Hossam <geek.kareem.hossam@gmail.com>
2024-02-26 12:53:51 +0200

    Time: 1405 ms (31.15%), Space: 0 (100.00%) - LeetHub

---
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -0,0 +1,17 @@
+SELECT conf_table.user_id, COALESCE(ROUND(confirmed_requests/requests,2),0) AS confirmation_rate
+FROM (
+    SELECT s.user_id, COUNT(action) confirmed_requests
+    FROM signups s
+    LEFT JOIN confirmations c
+        ON s.user_id = c.user_id
+        AND action = 'confirmed'
+    GROUP BY s.user_id
+          ) AS conf_table
+JOIN (
+    SELECT s.user_id, COUNT(action) requests
+    FROM signups s
+    LEFT JOIN confirmations c
+        ON s.user_id = c.user_id
+    GROUP BY s.user_id
+) AS req_table
+USING (user_id)
